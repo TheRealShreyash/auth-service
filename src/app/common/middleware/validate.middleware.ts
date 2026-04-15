e26f1182b474a746cd5f35a93bd601f5fe50cb2f
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-15 23:46:55 +0530

    fix: validation middleware response

--- src/app/common/middleware/validate.middleware.ts
+++ src/app/common/middleware/validate.middleware.ts
@@ -1,18 +1,23 @@
 import type { NextFunction, Request, Response } from "express";
 import type { ZodObject } from "zod";
 import ApiError from "../utils/api-error";
+import ApiResponse from "../utils/api-response";
 
 const validate = (schema: ZodObject<any>) => {
-  return (req: Request, _: Response, next: NextFunction) => {
-    const result = schema.safeParse(req.body);
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = schema.safeParse(req.body);
 
-    if (!result.success) {
-      const errors = result.error.issues.map((e) => e.message).join("; ");
-      throw ApiError.badRequest(errors);
+      if (!result.success) {
+        const errors = result.error.issues.map((e) => e.message).join("; ");
+        throw ApiError.badRequest(errors);
+      }
+
+      req.body = result.data;
+      next();
+    } catch (error) {
+      ApiResponse.error(res, error);
     }
-
-    req.body = result.data;
-    next();
   };
 };
 
